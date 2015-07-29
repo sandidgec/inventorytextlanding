dc9ab2ce2784b06f2e686c2cae307f78d3ecda94
Charles Sandidge <sandidgec@gmail.com>
2015-07-29 16:21:25 -0600

    Added landing page & image

--- index.php
+++ index.php
@@ -1,13 +1,43 @@
 <!DOCTYPE html>
-<html Lang="en">
+<html lang="en">
 	<head>
-		<link rel="stylesheet" type="text/css" href="mystyle.css">
-		<meta charset="UTF-8">
-		<title> inventoryTEXT Coming Soon!</title>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- ////////////////////////////////////////////////
+		 //// LINK TO YOUR CUSTOM CSS FILES HERE
+		 //// <link type="text/css" href="css/style.css" rel="stylesheet"/>
+		 ///////////////////////////////////////////////////// -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>Bootstrap 101 Template (DDC Version!)</title>
 	</head>
 	<body>
-		<h1> YOUR NEW WAREHOUSE EMPLOYEE IS COMING SOON!</h1>
-		<img src="./images/phoneExample.png" alt="Phone Example 1">
+		<div class="container">
+			<h1> Your new warehouse employee is Coming Soon! </h1>
+			<img src="./images/phoneExample.png" alt="Phone Example 1">
+		</div>
 	</body>
 </html>
 
