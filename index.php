1635468f934f038c7536bafa6cb28a1e57b87bb5
Charles Sandidge <sandidgec@gmail.com>
2015-07-29 15:03:59 -0600

    Added landing page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html Lang="en">
+	<head>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+		<meta charset="UTF-8">
+		<title> inventoryTEXT Coming Soon!</title>
+	</head>
+	<body>
+		coming soon!
+	</body>
+</html>
+
